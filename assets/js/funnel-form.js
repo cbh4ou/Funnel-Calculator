846b6aba2a72dee715bb70aa8c5ce7fa1dca1e64
Connor Hatcher <cbh4ou@gmail.com>
2020-10-28 15:25:59 -0500

    Added FE Product select to form

--- assets/js/funnel-form.js
+++ assets/js/funnel-form.js
@@ -33,16 +33,37 @@
         $(".step3").removeClass('d-none');
 
     });
-    $(".open3").click(function () {
+    $("#prodselect").change(function () {
+        if ($(this).val() > 1) {
+            var i;
+            var prodnum = 2
 
-        // optional
-        // used delay form submission for a seccond and show a loader image
-        $("#loader").show();
-        setTimeout(function () {
-            $("#basicform").html('<h2>Thanks for your time.</h2>');
-        }, 1000);
-        // Remove this if you are not using ajax method for submitting values
-        return false;
+            
 
+            for (i = $(this).val(); i > 1; i--) {
+                var div = String.format('<div class="row"><div class="col-xl-6"><div class="form-group"><label for="prods">FE Product Option {0}</label><input type="url" class="form-control" id="url" placeholder=""></div></div><div class="col-xl-4"><div class="form-group"><label for="prods">Estimated Take Rate %</label><input type="url" class="form-control" id="url" placeholder=""></div></div></div>', prodnum);
+                $(".feprods").append(div)
+                prodnum++;
+            }
+
+
+
+        }
     });
+    String.format = function () {
+        // The string containing the format items (e.g. "{0}")
+        // will and always has to be the first argument.
+        var theString = arguments[0];
+
+        // start with the second argument (i = 1)
+        for (var i = 1; i < arguments.length; i++) {
+            // "gm" = RegEx options for Global search (more than one instance)
+            // and for Multiline search
+            var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
+            theString = theString.replace(regEx, arguments[i]);
+        }
+
+        return theString;
+    }
+
 })(jQuery);
